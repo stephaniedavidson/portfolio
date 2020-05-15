93ff9b364d0f6053abd1edc6a1d1545dce5968f1
Steph Davidson <stephcdavidson@gmail.com>
2020-05-14 21:50:43 -0400

    with each commit my code becomes more beautiful and elegant.

--- src/templates/project-template.js
+++ src/templates/project-template.js
@@ -17,6 +17,7 @@ class ProjectTemplate extends React.Component {
         />
 
         <h1>{post.frontmatter.title}</h1>
+        <h2>test i am the slug {post.frontmatter.slug}</h2>
         <p>{post.frontmatter.date}</p>
         <MDXRenderer>{post.body}</MDXRenderer>
       </Layout>
